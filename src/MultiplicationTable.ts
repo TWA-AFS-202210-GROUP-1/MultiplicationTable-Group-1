14c5fc451d9a315d0217563c5a11a01cfa591bf6
Xu DU <XDu3@slb.com>
2022-10-25 17:19:19 +0800

    feat: can generate multiplication table

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,49 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public render(start: number, end: number): string {
+    if (!this.isnNumbersValid(start, end)) {
+      return ''
+    }
+
+    return this.generateMultiplicationTable(start, end)
+  }
+
+  isnNumbersValid(start: number, end: number): boolean {
+    return this.isStartNotGreaterEnd(start, end) && this.isStartEndOutRange(start, end)
+  }
+
+  isStartNotGreaterEnd(start: number, end: number): boolean {
+    return start <= end
+  }
+
+  isStartEndOutRange(start: number, end: number): boolean {
+    return start >= 1 && end <= 10
+  }
+
+  generateMultiplicationTable(start: number, end: number): string {
+    let table = ''
+    for (let i = start; i <= end; i++) {
+      table += this.generateLine(start, i)
+      if (i < end) {
+        table += '\n'
+      }
+    }
+
+    return table
+  }
+
+  generateLine(columnStart: number, columnEnd: number): string {
+    let line = ''
+    for (let i = columnStart; i <= columnEnd; i++) {
+      line += this.generateItem(i, columnEnd)
+      if (i < columnEnd) {
+        line += '  '
+      }
+    }
+
+    return line
+  }
+
+  generateItem(numberA: number, numberB: number): string {
+    return `${numberA}*${numberB}=${numberA * numberB}`
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,16 +1,60 @@
-import {MultiplicationTable} from '../src/MultiplicationTable'
+import { MultiplicationTable } from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
-  it('should render multiplication table of (1, 1)', () => {
+  it('should render multiplication table of (1, 2)', () => {
     // given
     const table = new MultiplicationTable()
     const start = 1
-    const end = 1
+    const end = 2
 
-    //when
+    // when
     const rendered = table.render(start, end)
 
     //then
-    expect(rendered).toBe('1*1=1')
+    const expectStr = '1*1=1\n1*2=2  2*2=4'
+
+    expect(rendered).toBe(expectStr)
+  })
+
+  it('should render empty string when give (2,1)', () => {
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    // when
+    const rendered = table.render(start, end)
+
+    //then
+    const expectStr = ''
+
+    expect(rendered).toBe(expectStr)
+  })
+
+  it('should render empty string when give (0,2)', () => {
+    const table = new MultiplicationTable()
+    const start = 0
+    const end = 2
+
+    // when
+    const rendered = table.render(start, end)
+
+    //then
+    const expectStr = ''
+
+    expect(rendered).toBe(expectStr)
+  })
+
+  it('should render empty string when give (2,11)', () => {
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 11
+
+    // when
+    const rendered = table.render(start, end)
+
+    //then
+    const expectStr = ''
+
+    expect(rendered).toBe(expectStr)
   })
 })
