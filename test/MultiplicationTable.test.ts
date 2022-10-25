2cc1beae79d67b4ba6c163dffb961b3ba26c3ed1
zwang <zwang91@slb.com>
2022-10-25 18:13:31 +0800

    feat: add a test of (2,2)

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -14,7 +14,19 @@ describe('Multiplication Table', () => {
     expect(rendered).toBe(`2*2=4
 2*3=6  3*3=9
 2*4=8  3*4=12  4*4=16`)
-    // expect(rendered).toBe("2*2=4")
+  })
+
+  it('should render multiplication table of (2, 2)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 2
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('2*2=4')
   })
 
   it('should return "" of (0, 1)', () => {
@@ -26,6 +38,7 @@ describe('Multiplication Table', () => {
     expect(rendered).toBe("")
   })
 
+  
   // it('should return "2*4  3*4  4*4"', () => {
   //   const table = new MultiplicationTable()
   //   const start = 2
