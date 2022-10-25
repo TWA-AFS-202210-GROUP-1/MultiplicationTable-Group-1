6fa5fc4c5055ec2ea3ab93608e43e5c8fd4085d7
Minghao QU <MQu2@slb.com>
2022-10-25 21:40:53 +0800

    feat: refactor

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,46 +1,46 @@
-import { MultiplicationTable } from "../src/MultiplicationTable";
+import { MultiplicationTable } from '../src/MultiplicationTable'
 
-describe("Multiplication Table", () => {
-  it("should render multiplication table of (1, 1)", () => {
+describe('Multiplication Table', () => {
+  it('should render multiplication table of (2, 4)', () => {
     // given
-    const table = new MultiplicationTable();
-    const start = 1;
-    const end = 1;
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
 
     // when
-    const rendered = table.render(start, end);
+    const rendered = table.render(start, end)
 
     // then
-    expect(rendered).toBe("1*1=1");
-  });
+    expect(rendered).toBe('2*4=8')
+  })
 
   it('should return true given numbers [1, 5]', () => {
-    const table = new MultiplicationTable();
-    const start = 1;
-    const end = 5;
+    const table = new MultiplicationTable()
+    const start = 1
+    const end = 5
 
-    const isValid = table.isValid(start, end);
+    const isValid = table.isValid(start, end)
 
-    expect(isValid).toBeTruthy();
-  });
+    expect(isValid).toBeTruthy()
+  })
 
   it('should return false given numbers [5, 1]', () => {
-    const table = new MultiplicationTable();
-    const start = 5;
-    const end = 1;
+    const table = new MultiplicationTable()
+    const start = 5
+    const end = 1
 
-    const isValid = table.isValid(start, end);
+    const isValid = table.isValid(start, end)
 
-    expect(isValid).toBeFalsy();
-  });
+    expect(isValid).toBeFalsy()
+  })
 
   it('should return false given numbers [0, 11]', () => {
-    const table = new MultiplicationTable();
-    const start = 0;
-    const end = 11;
+    const table = new MultiplicationTable()
+    const start = 0
+    const end = 11
 
-    const isValid = table.isValid(start, end);
+    const isValid = table.isValid(start, end)
 
-    expect(isValid).toBeFalsy();
-  });
-});
+    expect(isValid).toBeFalsy()
+  })
+})
