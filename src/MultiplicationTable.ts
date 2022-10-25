f2e3a93934501ddcdb2733b3d708954f2779fb3a
Minghao QU <MQu2@slb.com>
2022-10-25 17:26:23 +0800

    feat: change package.json

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,10 +1,12 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string {
-    const isValid: boolean = this.isValid(start, end)
-    return '1*1=1'
+    const isValid: boolean = this.isValid(start, end);
+    return "1*1=1";
   }
 
   private isValid(start: number, end: number): boolean {
-    return true
+    const isValid = isValidRange(start, end);
+
+    return isValid;
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,16 +1,16 @@
-import { MultiplicationTable } from '../src/MultiplicationTable'
+import { MultiplicationTable } from "../src/MultiplicationTable";
 
-describe('Multiplication Table', () => {
-  it('should render multiplication table of (1, 1)', () => {
+describe("Multiplication Table", () => {
+  it("should render multiplication table of (1, 1)", () => {
     // given
-    const table = new MultiplicationTable()
-    const start = 1
-    const end = 1
+    const table = new MultiplicationTable();
+    const start = 1;
+    const end = 1;
 
     // when
-    const rendered = table.render(start, end)
+    const rendered = table.render(start, end);
 
     // then
-    expect(rendered).toBe('1*1=1')
-  })
-})
+    expect(rendered).toBe("1*1=1");
+  });
+});
