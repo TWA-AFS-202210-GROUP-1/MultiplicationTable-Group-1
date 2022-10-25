874293c6728aea924796a54ca9181d126e4bb9b7
Minghao QU <MQu2@slb.com>
2022-10-25 17:20:09 +0800

    feat: add test function

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,10 +1,10 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string {
-    const isValid: boolean = this.isValid(start, end);
-    return "1*1=1";
+    const isValid: boolean = this.isValid(start, end)
+    return '1*1=1'
   }
 
   private isValid(start: number, end: number): boolean {
-    return true;
+    return true
   }
 }
